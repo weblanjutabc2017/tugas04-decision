e4f23198ff2acc3b31469510be212d7d2a085e09
Hesya <hesya.megita@gmail.com>
2017-08-28 12:39:43 +0700

    menambahkan file tugas 02

---
+++ 04_AB_11536.php
@@ -0,0 +1,18 @@
+<?
+$a = String("x");
+
+if($a = "A" || "B" || "A-" || "A/B" || "B+" || "B-" || "B/C"){
+	echo "LULUS";
+	break;
+}else if($a = "C+" || "C" || "C-" || "C/D"){
+	echo "LULUS SEBAIKNYA DIULANG";
+	break;
+}else if($a = "D+" || "D"){
+	echo "LULUS DAN WAJIB DIULANG";
+	break;
+}else{
+	echo "WAJIB DIULANG";
+	break;
+}
+?>
+
