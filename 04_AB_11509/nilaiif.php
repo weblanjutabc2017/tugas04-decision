dc891b7d7d256245448a735b04a89e07393459b0
ajisett <aryantoasa@gmail.com>
2017-08-28 13:08:52 +0700

    Menambahkan file tugas

---
+++ 04_AB_11509/nilaiif.php
@@ -0,0 +1,34 @@
+<?php
+	$nilai;
+	if($nilai == "A"){
+		echo "LULUS";
+	}elseif($nilai == "A-"){
+		echo "LULUS";
+	}elseif($nilai == "A/B"){
+		echo "LULUS";
+	}elseif($nilai == "B+"){
+		echo "LULUS";
+	}elseif($nilai == "B"){
+		echo "LULUS";
+	}elseif($nilai == "B-"){
+		echo "LULUS";
+	}elseif($nilai == "B/C"){
+		echo "LULUS";
+	}elseif($nilai == "C+"){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($nilai == "C-"){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($nilai == "C/D"){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($nilai == "C"){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($nilai == "D+"){
+		echo "LULUS DAN WAJIB DIULANG";
+	}elseif($nilai == "D"){
+		echo "LULUS DAN WAJIB DIULANG";
+	}elseif($nilai == "E"){
+		echo "TIDAK LULUS";
+	}else{
+		echo "NILAI SALAH !!!";
+	}
+?>
